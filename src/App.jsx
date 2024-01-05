985f6484812db933d9b9da83e461b78818a2518d
AndrijaBosnjak <andrijab2@gmail.com>
2024-01-05 12:48:46 +0100

    add counter state, wrongResult, Play again button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
 import "./App.css";
 
@@ -7,24 +7,74 @@ function App() {
   const [enteredWord, setEnteredWord] = useState("");
   const [isWordEntered, setIsWordEntered] = useState(false);
   const [enteredLetter, setEnteredLetter] = useState("");
-  const [wrongAttemptCounter, setWrongAttemptCounter] = useState(0);
+  const [wrongGuessesCounter, setWrongGuessesCounter] = useState(0);
+  const [correctGuesses, setCorrectGuesses] = useState([]);
+  const [wrongGuesses, setWrongGuesses] = useState([]);
 
   const onStartGame = () => {
     setIsGameStart(true);
   };
 
+  const enteredWordArray = [...enteredWord];
+
+  // const equalsCheck = (enteredWordArray, correctGuesses) => {
+  //   return JSON.stringify(enteredWordArray) === JSON.stringify(correctGuesses);
+  // };
+
+  const maskedEnteredWord = [...enteredWord]
+    .map((letter) => (correctGuesses.includes(letter) ? letter : "_"))
+    .join(" ");
+
   const onEnterWord = (event) => {
-    console.log(enteredWord);
-    // const lettersArray = [...enteredWord];
-    // const lettersArrayLength = lettersArray.length;
-    // console.log(lettersArray, lettersArrayLength);
     setIsWordEntered(true);
   };
 
   const onEnterLetter = (event) => {
-    console.log(enteredLetter);
+    if (enteredWord.includes(enteredLetter)) {
+      setCorrectGuesses([...correctGuesses, enteredLetter]);
+    } else {
+      setWrongGuesses([...wrongGuesses, enteredLetter]);
+      setWrongGuessesCounter(wrongGuessesCounter + 1);
+    }
     setEnteredLetter("");
-  }
+  };
+
+  const onPlayAgain = () => {
+    setIsGameStart(true);
+    setEnteredWord("");
+    setIsWordEntered(false);
+    setWrongGuessesCounter(0);
+    setCorrectGuesses([]);
+    setWrongGuesses([]);
+  };
+
+  const showResults = useMemo(() => {
+    if (wrongGuessesCounter == 5) {
+      setIsGameStart(false);
+
+      return (
+        <>
+          <h1>{[...enteredWord].join(" ")}</h1>
+          <h2>Popušio si!!</h2>
+          <button onClick={() => onPlayAgain()}>Play again</button>
+        </>
+      );
+    }
+
+    }, [wrongGuessesCounter]);
+
+  // const showCorrectResult = () => {
+  //   if (!maskedEnteredWord.includes("_")) {
+      
+  //     return (
+  //       <>
+  //         <h1>{[...enteredWord].join(" ")}</h1>
+  //         <h2>Čestitam!</h2>
+  //         <button onClick={() => onPlayAgain()}>Play again</button>
+  //       </>
+  //     );
+  //   }
+  // };
 
   if (!isWordEntered) {
     return (
@@ -32,7 +82,7 @@ function App() {
         {!isGameStart && <button onClick={onStartGame}>Play</button>}
         {isGameStart && (
           <>
-            <p>Unesi riječ od najmanje 3 slova:</p>
+            <h2>Unesi riječ od najmanje 3 slova:</h2>
             <form>
               <input
                 type="text"
@@ -52,20 +102,28 @@ function App() {
 
   return (
     <>
-      <h3>Broj netočnih pokušaja :{" "}{wrongAttemptCounter}</h3>
-      <p>Unesi jedno slovo:</p>
-      <p>{[...enteredWord].fill("_").join(" ")}</p>
-      <form>
-        <input
-          type="text"
-          id="word"
-          name="word"
-          placeholder="dozvoljena su samo slova"
-          value={enteredLetter}
-          onChange={(e) => setEnteredLetter(e.target.value)}
-        />
-      </form>
-      <button onClick={onEnterLetter}>PROVJERI</button>
+      {isGameStart && (
+        <>
+          <h1>{maskedEnteredWord}</h1>
+          <h3>Broj netočnih pokušaja : {wrongGuessesCounter} / 5</h3>
+          <p>Krivi odgovori: {wrongGuesses.join(" , ")} </p>
+          <p>Unesi jedno slovo:</p>
+          <form>
+            <input
+              type="text"
+              id="word"
+              name="word"
+              placeholder="dozvoljena su samo slova"
+              value={enteredLetter}
+              onChange={(e) => setEnteredLetter(e.target.value)}
+            />
+          </form>
+          <button onClick={onEnterLetter}>PROVJERI</button>
+        </>
+      )}
+      {showResults}
+      {!maskedEnteredWord.includes("_") && <p>Pobjedio si!</p>}
+      {/* {showCorrectResult} */}
     </>
   );
 }
